c268746b0c9eb7834032bb073175426706f90bb9
jeffersoncostaIfma <jeffersoncosta@acad.ifma.edu.br>
2020-10-24 14:07:37 -0300

    finalizado

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="row justify-content-around">
+
+      <Conversor nome_moeda1="Euro" nome_moeda2="Real " valor_moeda1="1" valor_moeda2="6.67"/>
+      <Conversor nome_moeda1="Euro" nome_moeda2="Dolar" valor_moeda1="1" valor_moeda2="1.19"/>
+      <Conversor nome_moeda1="Dolar" nome_moeda2="Real" valor_moeda1="1" valor_moeda2="5.62"/>
+      <Conversor nome_moeda1="Yen" nome_moeda2="Real" valor_moeda1="1" valor_moeda2="0.054"/>
+      <Conversor nome_moeda1="Euro" nome_moeda2="Yen" valor_moeda1="1" valor_moeda2="124.21"/>
+    </div>
+    <div class="row  justify-content-around">
+          <Conversor nome_moeda1="Real" nome_moeda2="Euro " valor_moeda1="1" valor_moeda2="0.15"/>
+          <Conversor nome_moeda1="Dolar" nome_moeda2="Euro" valor_moeda1="1" valor_moeda2="0.84"/>
+          <Conversor nome_moeda1="Real" nome_moeda2="Dolar" valor_moeda1="1" valor_moeda2="0.18"/>
+          <Conversor nome_moeda1="Real" nome_moeda2="Yen" valor_moeda1="1" valor_moeda2="18.64"/>
+          <Conversor nome_moeda1="Yen" nome_moeda2="Euro" valor_moeda1="1" valor_moeda2="0.081"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Conversor from './components/Conversor.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Conversor
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/Conversor.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="hello">
+        <div class="col-sm-12 ">
+            <div class="card ">
+                <div class="card-header">
+                    {{nome_moeda1}} / {{nome_moeda2}}
+                </div>
+                <div class="card-body">
+                    <label for="">{{nome_moeda1}}:</label>
+                    <input v-model="value"  type="number" class="form-control">
+                    <label for="">{{nome_moeda2}}:</label>
+                    <input disabled v-bind:value="valor_moeda2"   type="number" class="form-control">
+                    <h1 class="text-center">{{multipliedValue}}</h1>
+                </div>
+            </div>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Conversor',
+  props: {
+    nome_moeda1:String,
+    nome_moeda2:String,
+    valor_moeda2:Number
+    
+  },
+  data:function(){
+      return{
+          value:1,
+        //   valor_moeda2,
+      }
+  },
+  computed:{
+        multipliedValue () {
+        console.log (this.valor_moeda2);
+        return (this.value * this.valor_moeda2);
+   }
+  },
+  methods:{
+    //   converte(valor_moeda1 , valor_moeda2){
+    //     //   const convertido = (parseFloat(valor_moeda) * parseFloat(valor_moeda2));
+    //     //   this.valor_moeda.push(convertido);
+          
+    //   },
+  }
+}
+</script>
+
+<style scoped>
+button{
+    margin-top: 10px;
+}
+</style>
